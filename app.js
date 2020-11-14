00cf1ed412aa4276d613e93655da568f2e31d03e
itsLyLee <lyylee00@gmail.com>
2020-11-14 22:54:21 +0100

    todo event

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+
 //Event Listeners
+todoButton.addEventListener("click", addTodo);
 
 //Functions
