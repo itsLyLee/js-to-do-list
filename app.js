11d01d840b65639810900ffef70dd45dda3392b0
itsLyLee <lyylee00@gmail.com>
2020-11-19 18:23:45 +0100

    function checkmark

--- app.js
+++ app.js
@@ -42,4 +42,10 @@ function deleteCheck(e) {
     const todo = item.parentElement;
     todo.remove();
   }
+
+  //Checkmark
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
 }
