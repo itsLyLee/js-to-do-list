8be4e18b7a518d27ea4f6df1609acc571a0c2661
itsLyLee <lyylee00@gmail.com>
2020-11-24 23:49:47 +0100

    filtertodo function

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
-
+const filterOption = document.querySelector(".filter-todo");
 //Event Listeners
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
@@ -52,4 +52,9 @@ function deleteCheck(e) {
     const todo = item.parentElement;
     todo.classList.toggle("completed");
   }
+}
+
+function filterTodo(e) {
+
+}
 }
