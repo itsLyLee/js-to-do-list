256e033ecedfd1dc34e56f5a60bb012d1c2a7397
itsLyLee <lyylee00@gmail.com>
2020-11-29 22:25:03 +0100

    event listener document

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 const filterOption = document.querySelector(".filter-todo");
 //Event Listeners
+document.addEventListener("DOMContentLoaded", getTodos);
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
 filterOption.addEventListener("click", filterTodo);
